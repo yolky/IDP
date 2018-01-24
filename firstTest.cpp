72c81275c861da474a56ffe16a47d5933571675a
N. Loo <nl355@ew117.eng.cam.ac.uk>
2018-01-24 09:37:24 +0000

    shit commit please ignore

---
+++ firstTest.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+#include <robot_instr.h>
+#include <robot_link.h>
+#define ROBOT_NUM  16                         // The id number (see below)
+#include <robot_delay.h>
+#include <stopwatch.h>
+robot_link  rlink;                            // datatype for the robot link
+int main ()
+{
+	int   val;                                // data from microprocessor
+	#ifdef __arm__
+		if (!rlink.initialise ("127.0.0.1")) {          // setup for local hardware
+	#else
+	   if (!rlink.initialise (ROBOT_NUM)) { // setup the link
+	#endif
+		cout << "Cannot initialise link" << endl;
+		rlink.print_errs("    ");
+		return -1;
+	}
+	val = rlink.request (TEST_INSTRUCTION);   // send test instruction
+		if (val == TEST_INSTRUCTION_RESULT) {     // check result
+		cout << "Test passed" << endl;
+		
+		rlink.command (MOTOR_1_GO, 127); //RED = 1
+		rlink.command (MOTOR_2_GO, 127); //GREEN = 2
+		delay(10000);
+		
+		//38.5ms for writing
+		//5ms for reading
+		//2ms reading on robot
+		
+		
+		int value = 255;
+		stopwatch watch;
+		watch.start();
+		for(int i=0; i< 100; i ++){
+			rlink.command(WRITE_PORT_5, value);
+		}
+		int time = watch.read();
+		watch.stop();
+		
+		cout<< time<< endl;
+		
+		return 0;                             // all OK, finish
+	}
+	else if (val == REQUEST_ERROR) {
+		cout << "Fatal errors on link:" << endl;
+		rlink.print_errs();
+	}
+	else
+		cout << "Test failed (bad value returned)" << endl;
+	return -1;                                // error, finish
+
+}
